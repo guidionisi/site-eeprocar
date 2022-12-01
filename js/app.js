1af36e6cfaf730634d0efb00cabd1a0f28668a38
guidionisi <guidionisi@gmail.com>
2022-12-01 16:25:09 -0300

    Melhorias na responsividade

--- js/app.js
+++ js/app.js
@@ -13,3 +13,20 @@ $(".header-menu a[href^='#']").click(function (e) {
     500
   );
 });
+
+// escolha do item da UEP
+$(".uep-item").click(function (e) {
+  e.preventDefault();
+
+  const textosItens = $(".uep-descricao"); //recupera todas as divs com descrição das ueps
+  const itemId = $(this).attr("href"); // recupera apenas o item clicado
+
+  $(textosItens).addClass("none"); //remove da tela todas os textos
+  $(itemId).removeClass("none"); //coloca na tela apenas o texto do item clicado
+});
+
+// menu mobile
+
+$(".mobile-menu-icon").click(function () {
+  $(".mobile-menu").toggle("none");
+});
